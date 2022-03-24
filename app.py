8545109527786a521718c7ac4fe005bc69df8d81
Ashish <ashish.patil401@gmail.com>
2022-03-24 17:33:54 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])  # To render Homepage
+def home_page():
+    return render_template('index.html')
+
+
+@app.route('/math', methods=['POST'])  # This will be called from UI
+def math_operation():
+    if request.method == 'POST':
+        operation = request.form['operation']
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if operation == 'add':
+            r = num1 + num2
+            result = 'the sum of ' + str(num1)+' and '+str(num2) + ' is ' + str(r)
+        if operation == 'subtract':
+            r = num1 - num2
+            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if operation == 'multiply':
+            r = num1 * num2
+            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if operation == 'divide':
+            r = num1 / num2
+            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+        if operation == 'f_division':
+            r = num1 // num2
+            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+    return render_template('results.html', result=result)
+
+
+@app.route('/via_postman', methods=['POST'])  # for calling the API from Postman/SOAPUI
+def math_operation_via_postman():
+    if request.method == 'POST':
+        operation = request.json['operation']
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        if operation == 'add':
+            r = num1 + num2
+            result = 'the sum of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if operation == 'subtract':
+            r = num1 - num2
+            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if operation == 'multiply':
+            r = num1 * num2
+            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if operation == 'divide':
+            r = num1 / num2
+            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+        return jsonify(result)
+
+
+@app.route('/ashish', methods=['POST'])  # for calling the API from Postman/SOAPUI
+def sum_operation_via_postman():
+    if request.method == 'POST':
+        operation = request.json['operation']
+        num0 = int(request.json['num0'])
+        num1 = int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        if operation == 'add':
+            r = num0 + num1 + num2
+            result = 'The sum of ' + str(num0) + ' and ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+
+        return jsonify(result)
+
+
+@app.route('/sudh', methods=['POST'])  # for calling the API from Postman/SOAPUI
+def math_sudh():
+    if request.method == 'POST':
+        name = request.json['name']
+        emailid = request.json['emailid']
+        phonenumber = request.json['phonenumber']
+
+        return jsonify(name + emailid + str(phonenumber))
+
+
+@app.route('/sudh_function')
+def url_test():
+
+    test1 = request.args.get('val1')
+    test2 = request.args.get('val2')
+    test3 = int(test1) + int(test2)
+
+    return '''<h1> My result is : {}<h1>'''.format(test3)
+
+
+if __name__ == '__main__':
+    app.run()
